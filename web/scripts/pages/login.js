a8c7fa738870fbde4d4b9b7cf608979711db60b1
Coll147 <aaroncoll147@gmail.com>
2025-12-08 19:54:54 +0100

    launch login function pressing enter

--- web/scripts/pages/login.js
+++ web/scripts/pages/login.js
@@ -20,3 +20,14 @@ function login(id) {
         showText("Contraseña Incorrecta")
     }
 }
+
+// Para activar la función si pulso enter
+// https://stackoverflow.com/questions/7060750/detect-the-enter-key-in-a-text-input-field
+document.querySelectorAll(".input-group").forEach(input => { 
+    // Meto el listener a los dos grupos de input aunque ahora solo se puede escribir en password
+    input.addEventListener("keyup", (e) => {
+        if (e.key === "Enter") {
+            login(e.target.id);
+        }
+    });
+});
