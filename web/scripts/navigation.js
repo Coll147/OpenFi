f89b1473e4f67dbc3446776ef9cffb13ce4bec55
Coll147 <aaroncoll147@gmail.com>
2025-12-16 21:05:18 +0100

    fix navigation again

--- web/scripts/navigation.js
+++ web/scripts/navigation.js
@@ -1,19 +1,12 @@
-// Detectar ruta (que luego github pages llora)
-let basePath = ''
-const pathParts = window.location.pathname.split('/')
-if (pathParts[1]) {
-  basePath = '/' + pathParts[1]
-}
-window.BASE_PATH = basePath
-
-
+// Detectar la base de la ruta (GitHub Pages o local)
+let BASE_PATH = window.location.pathname.startsWith('/OpenFi') ? '/OpenFi' : '/web'
 
 
 // Actualizar contenido
 function updatePage(page, element, push = true) {
   // Actualizar iframe
   const frame = document.getElementById('content-frame')
-  const src = `${BASE_PATH}/web/dashboard/${page}.html`
+  const src = `${BASE_PATH}/dashboard/${page}.html`
   frame.contentWindow.location.replace(src)
 
   // Actualizar NAV
@@ -22,7 +15,7 @@ function updatePage(page, element, push = true) {
 
   // Actualizar URL
   if (push) { // solo cuando se navegue (no direct load)
-    history.pushState({ page: page },  "", `${window.BASE_PATH}/?page=${page}`)
+    history.pushState({ page: page },  "", `${BASE_PATH}/?page=${page}`)
   }
 
   // Ajustar CSS
