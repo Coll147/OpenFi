0050a3119c94bd600004472b9fba724c3d6ba310
Coll147 <aaroncoll147@gmail.com>
2025-12-20 23:52:35 +0100

    improve code

--- web/initial-setup/setup.js
+++ web/initial-setup/setup.js
@@ -5,21 +5,28 @@ document.getElementById('show-password2').addEventListener('click', () => {
 
 
 async function setup() {
-  // Take variables
+  // Get variables
   let account = JSON.parse(localStorage.getItem("account")) // Load BD
+
+  const language = document.getElementById('selector_language').value
+  const background = document.getElementById('selector_background').value
+  const theme = document.getElementById('selector_theme').value
+
   const passwd1 = document.getElementById('passwd1').value
   const passwd2 = document.getElementById('passwd2').value
   const email = document.getElementById('email-set').value
 
   // Security checks
+  if (!language || !language || !language)
   if (!passwd1 || (passwd1 !== passwd2)) {
     console.log('Password not valid')
     return
   }
 
-  console.log(account[0].password)
+  account[0].language = language
+  account[0].background = background
+  account[0].theme = theme
   account[0].password = await sha256(passwd1)
-  console.log(account[0].password)
   account[0].email = email
 
   localStorage.setItem("account", JSON.stringify(account)) // Save DB
