c9f6c5802b9674b660991764fc047f90f501d39d
Coll147 <aaroncoll147@gmail.com>
2025-12-20 23:14:34 +0100

    improve debugging change password default value

--- web/data/db.js
+++ web/data/db.js
@@ -122,7 +122,7 @@
         {
             id: 0,
             username: "admin",
-            password: "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", // admin
+            password: "password not specified", // admin
             firsttime: "yes",
             email: "", // no default, you set it :)
             language: "english",
