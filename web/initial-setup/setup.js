eeb33abd7e12e3c4579b5cbbb1e1f64263542c3f
Coll147 <aaroncoll147@gmail.com>
2025-12-20 23:26:45 +0100

    update script flow cleaner code email now its saved on the db correctly

--- web/initial-setup/setup.js
+++ web/initial-setup/setup.js
@@ -3,61 +3,29 @@ document.getElementById('show-password2').addEventListener('click', () => {
     element.type = element.type === 'password' ? 'text' : 'password'
 })
 
-function setup() {
-  if (document.getElementById('passwd1').value === document.getElementById('passwd2').value) {
-    console.log('contraseñas coinciden')
-    changeValueSHA(0, 'passwd1', 'password') // set password
-    changeValue(0, 'email-set', 'email') // set email
-    log('User Setup', `OpenFi System`, 'Info', `Welcome to OpenFi`)
-  } else {
-    console.log('Las contraseñas no coinciden')
-  }
-  window.location.href = "../login.html"
-}
 
-// Funcion que guarda hash - solo se usa para la contraseña por ahora
-async function changeValueSHA(user_id, element, db_value) {
-  let account = JSON.parse(localStorage.getItem("account"))
+async function setup() {
+  // Take variables
+  let account = JSON.parse(localStorage.getItem("account")) // Load BD
+  const passwd1 = document.getElementById('passwd1').value
+  const passwd2 = document.getElementById('passwd2').value
+  const email = document.getElementById('email-set').value
 
-  const contenido = document.getElementById(element)?.value
-  if (!contenido) {
-    console.error('Contenido vacío')
-    document.getElementById('settingsPaswdchgAlertbox').textContent = 'Contenido vacío'
-    document.getElementById('settingsPaswdchgAlertbox').style.color = 'red'
+  // Security checks
+  if (!passwd1 || (passwd1 !== passwd2)) {
+    console.log('Password not valid')
     return
   }
 
-  const oldValue = account[user_id][db_value]
-  const newHash = await sha256(contenido)
+  console.log(account[0].password)
+  account[0].password = await sha256(passwd1)
+  console.log(account[0].password)
+  account[0].email = email
 
-  console.log(`Old ${db_value}: ${oldValue}`)
-  console.log(`New ${db_value}: ${newHash}`)
+  localStorage.setItem("account", JSON.stringify(account)) // Save DB
 
-  account[user_id][db_value] = newHash
-
-  localStorage.setItem("account", JSON.stringify(account))
-
-  log(`${db_value} changed`,'OpenFi Account','Warn',`${db_value} de ${account[user_id].username} ha sido configurada.`)
-
-  console.log('Password updated correctly')
-  console.log(`Old ${db_value}: ${oldValue}`)
-  console.log(`New ${db_value}: ${newHash}`)
-}
+  log('User Setup', `OpenFi System`, 'Info', `Setup completed. Welcome  to  OpenFi`)
 
 
-
-// Funcion usada para cambiar valor
-function changeValue(user_id, element_id, db_value) {
-  let account = JSON.parse(localStorage.getItem("account")) // cargar localstorage
-
-  const contenido = document.getElementById(element_id).value
-  if (contenido == undefined) { 
-    console.error('Contenido vacío')
-    return 
-  }
-  console.log(`setup.js > changeValue: Cambiado ${db_value} a ${contenido}`)
-
-  account[user_id][db_value] = contenido
-
-  localStorage.setItem("account", JSON.stringify(account)) // guardar localstorage
+  window.location.href = "../login.html"
 }
