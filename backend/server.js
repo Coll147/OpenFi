7636262962f653e65f7dbc9243ee2ba2798ed7eb
Coll147 <aaroncoll147@gmail.com>
2026-01-11 14:39:55 +0100

    trying to understand express

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,22 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
 
+// Ruta para obtener datos
+app.get("/api/devices", (req, res) => {
+  const data = fs.readFileSync("./server/data.json", "utf-8");
+  res.json(JSON.parse(data).usuarios);
+});
 
+// Ruta para guardar datos
+app.post("/api/devices", (req, res) => {
+  const nuevoUsuario = req.body;
+
+  const data = JSON.parse(fs.readFileSync("./server/data.json", "utf-8"));
+  data.usuarios.push(nuevoUsuario);
+
+  fs.writeFileSync("./server/data.json", JSON.stringify(data, null, 2));
+  res.json({ mensaje: "Usuario guardado" });
+});
 
 
 app.listen(PORT, () => {
