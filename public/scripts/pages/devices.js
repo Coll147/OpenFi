839e6e6e675b28a527655d569cbe4072a59e6680
Coll147 <aaroncoll147@gmail.com>
2026-01-25 13:26:02 +0100

    identation

--- public/scripts/pages/devices.js
+++ public/scripts/pages/devices.js
@@ -91,154 +91,151 @@ async function loadTable(){ // Creación de la tabla - de dispositivos
 
 
 function toggleMenu() {
-    // Enseñar menú
-    const element = document.getElementById("context-menu")
-    const actualState = element.style.display
+  // Enseñar menú
+  const element = document.getElementById("context-menu")
+  const actualState = element.style.display
 
-    let statusTable = document.getElementById("device-status")
-    let wifiTable = document.getElementById("wifi-status")
+  let statusTable = document.getElementById("device-status")
+  let wifiTable = document.getElementById("wifi-status")
 
-    switch (actualState){
-        case "none":
-            element.style.display = "block"
-            break;
+  switch (actualState){
+    case "none":
+      element.style.display = "block"
+      break;
 
-        case "block":
-            element.style.display = "none"
-            while (statusTable.firstChild) {
-                statusTable.removeChild(statusTable.firstChild)
-            }
-            while (wifiTable.firstChild) {
-                wifiTable.removeChild(wifiTable.firstChild)
-            }
-            break;
+    case "block":
+      element.style.display = "none"
+      while (statusTable.firstChild) {
+          statusTable.removeChild(statusTable.firstChild)
+      }
+      while (wifiTable.firstChild) {
+          wifiTable.removeChild(wifiTable.firstChild)
+      }
+      break;
 
-        default:
-            console.log('actions.js > toogleMenu > Error al cambiar, valor inesperado')
-            console.log(actualState)
-    }
+    default:
+      console.log('actions.js > toogleMenu > Error al cambiar, valor inesperado')
+      console.log(actualState)
+  }
 }
 
-function mountMenu(device){ // Mostrar menú extra de cada dispositivo
+async function mountMenu(device){ // Mostrar menú extra de cada dispositivo
+  
+  const devices = await loadDB('devices');
+  console.log(`enseñando menú para ${device}`)
+
+  // Crear filas de la tabla de <radios>
+  // comentado hasta que se implemente la api para recibir la infomación
+  // let wifiTable = document.getElementById("wifi-status")
+
+  // for (let i = 0; i < devices[device].radios.length; i++) {
+
+  //     const tbody = document.createElement("tbody")
+  //     const tr = document.createElement("tr")
+
+  //         let td1 = document.createElement("td") // radio name
+  //         td1.textContent = devices[device].radios[i].name
+  //         tr.appendChild(td1)
+
+  //         let td2 = document.createElement("td") // channel
+  //         td2.append(
+  //             document.createTextNode(devices[device].radios[i].channel),
+  //             document.createTextNode(" "),
+  //             document.createTextNode(devices[device].radios[i].bandwidth),
+  //             document.createElement("br"),
+  //             document.createTextNode(devices[device].radios[i].frequency)
+  //         );
+  //         tr.appendChild(td2)
+
+  //         let td3 = document.createElement("td") // wifi_standard
+  //         td3.textContent = devices[device].radios[i].wifi_standard
+  //         tr.appendChild(td3)
+
+  //         let td4 = document.createElement("td") // speed
+  //         td4.textContent = devices[device].radios[i].speed
+  //         tr.appendChild(td4)
+
+  //         let td5 = document.createElement("td") // clients
+  //         td5.textContent = devices[device].radios[i].clients
+  //         tr.appendChild(td5)
+
+  //     tbody.appendChild(tr)
+
+  //     wifiTable.appendChild(tbody)
+  // }
+
+
+  // Crear filas de la tabla de <status>
+  let statusTable = document.getElementById("device-status")
+
+  let tbody = document.createElement("tbody")
+
+  let tr = ""
+  let td1 = ""
+  let td2 = ""
+
+    tr = document.createElement("tr") // Nick
+      td1 = document.createElement("td")
+      td1.textContent = "Device Name"
+      tr.appendChild(td1)
     
-    let devices = JSON.parse(localStorage.getItem("devices")) // cargar localstorage
-    console.log(`enseñando menú para ${device}`)
+      td2 = document.createElement("td")
+      //td2.textContent = devices[device].nickname -- pendiente cambiarlo por unos create element pero por ahora funciona :v
+      td2.innerHTML = `<input type="text" id="device-nick" placeholder="${devices[device].nickname}" onblur="changeNick(${device})">`
+      tr.appendChild(td2)
+    tbody.appendChild(tr)
 
-    // Crear filas de la tabla de <radios>
-    let wifiTable = document.getElementById("wifi-status")
+    tr = document.createElement("tr") // Model
+      td1 = document.createElement("td")
+      td1.textContent = "Model"
+      tr.appendChild(td1)
+    
+      td2 = document.createElement("td")
+      td2.textContent = devices[device].model
+      tr.appendChild(td2)
+    tbody.appendChild(tr)
 
-    console.log(devices[device].radios.length)
+    tr = document.createElement("tr") // IP
+      td1 = document.createElement("td")
+      td1.textContent = "IP Addr."
+      tr.appendChild(td1)
+    
+      td2 = document.createElement("td")
+      td2.textContent = devices[device].ip
+      tr.appendChild(td2)      
+    tbody.appendChild(tr)
 
-    for (let i = 0; i < devices[device].radios.length; i++) {
+    tr = document.createElement("tr") // IP
+      td1 = document.createElement("td")
+      td1.textContent = "IP Addr."
+      tr.appendChild(td1)
+    
+      td2 = document.createElement("td")
+      td2.textContent = devices[device].ip
+      tr.appendChild(td2)
+    tbody.appendChild(tr)
 
-        const tbody = document.createElement("tbody")
-        const tr = document.createElement("tr")
-        
-            let td1 = document.createElement("td") // radio name
-            td1.textContent = devices[device].radios[i].name
-            tr.appendChild(td1)
-            
-            let td2 = document.createElement("td") // channel
-            td2.append(
-                document.createTextNode(devices[device].radios[i].channel),
-                document.createTextNode(" "),
-                document.createTextNode(devices[device].radios[i].bandwidth),
-                document.createElement("br"),
-                document.createTextNode(devices[device].radios[i].frequency)
-            );
-            tr.appendChild(td2)
+    tr = document.createElement("tr") // Version
+      td1 = document.createElement("td")
+      td1.textContent = "Firmware version"
+      tr.appendChild(td1)
+      td2 = document.createElement("td")
+      td2.textContent = devices[device].version
+      tr.appendChild(td2)
+    tbody.appendChild(tr)
 
-            let td3 = document.createElement("td") // wifi_standard
-            td3.textContent = devices[device].radios[i].wifi_standard
-            tr.appendChild(td3)
+    statusTable.addEventListener("click", (event) => {
+      event.stopPropagation()
+    })
 
-            let td4 = document.createElement("td") // speed
-            td4.textContent = devices[device].radios[i].speed
-            tr.appendChild(td4)
+    statusTable.appendChild(tbody)
 
-            let td5 = document.createElement("td") // clients
-            td5.textContent = devices[device].radios[i].clients
-            tr.appendChild(td5)
-            
-        tbody.appendChild(tr)
+    toggleMenu()
 
-        wifiTable.appendChild(tbody)
-    }
-
-
-    // Crear filas de la tabla de <status>
-    let statusTable = document.getElementById("device-status")
-
-    let tbody = document.createElement("tbody")
-
-    let tr = ""
-    let td1 = ""
-    let td2 = ""
-
-        tr = document.createElement("tr") // Nick
-
-            td1 = document.createElement("td")
-            td1.textContent = "Device Name"
-            tr.appendChild(td1)
-        
-            td2 = document.createElement("td")
-            //td2.textContent = devices[device].nickname -- pendiente cambiarlo por unos create element pero por ahora funciona :v
-            td2.innerHTML = `<input type="text" id="device-nick" placeholder="${devices[device].nickname}" onblur="changeNick(${device})">`
-            tr.appendChild(td2)
-        
-
-        tbody.appendChild(tr)
-
-        tr = document.createElement("tr") // Model
-
-            td1 = document.createElement("td")
-            td1.textContent = "Model"
-            tr.appendChild(td1)
-        
-            td2 = document.createElement("td")
-            td2.textContent = devices[device].model
-            tr.appendChild(td2)
-        
-
-        tbody.appendChild(tr)
-
-        tr = document.createElement("tr") // IP
-
-            td1 = document.createElement("td")
-            td1.textContent = "IP Addr."
-            tr.appendChild(td1)
-        
-            td2 = document.createElement("td")
-            td2.textContent = devices[device].ip
-            tr.appendChild(td2)
-        
-
-        tbody.appendChild(tr)
-
-        tr = document.createElement("tr") // Version
-
-            td1 = document.createElement("td")
-            td1.textContent = "Firmware version"
-            tr.appendChild(td1)
-        
-            td2 = document.createElement("td")
-            td2.textContent = devices[device].version
-            tr.appendChild(td2)
-        
-
-        tbody.appendChild(tr)
-
-        statusTable.addEventListener("click", (event) => {
-            event.stopPropagation()
-        })
-
-        statusTable.appendChild(tbody)
-
-        toggleMenu()
-}
 
 // Cerrar menús secundarios al pulsar dondesea
 document.body.addEventListener("click", function(data) {
-    console.log("Hiciste click en alguna parte del body");
-    toggleMenu(false)
+  console.log("Hiciste click en alguna parte del body");
+  toggleMenu(false)
 })
+}
