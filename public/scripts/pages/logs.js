e1bb9c00cd2f9f8f1971723e5e31b2f7fa815661
Coll147 <91793263+Coll147@users.noreply.github.com>
2026-03-05 09:36:37 +0100

    fix logs filter risk + The filterrisk variable wasnt getting the value from the options menu

--- public/scripts/pages/logs.js
+++ public/scripts/pages/logs.js
@@ -38,10 +38,10 @@ document.getElementById('time').addEventListener('click', () => {
 // Cargar y crear tabla
 async function loadTable(riskFilter, sortTime) {
   const logs = await loadDB('logs');
-  const tabla = document.getElementById("devices-tbody");
+  const tabla = document.getElementById('devices-tbody');
 
-  // Defaults correctos
-  if (!riskFilter) riskFilter = 'All';
+  // Default values
+  if (!riskFilter) riskFilter = document.getElementById('riskFilter').value;
   if (!sortTime) sortTime = 'ascendente';
 
   // Limpiar tabla
